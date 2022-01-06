7800f98e5e8522dd6d89b2de075ab3de4ed8ec43
kengoaraki <kengo.araki@human.ait.kyushu-u.ac.jp>
2022-01-06 14:28:10 +0900

    fix test.py for ST_ADA

--- ST_ADA/eval.py
+++ ST_ADA/eval.py
@@ -165,6 +165,38 @@ def eval_net_trg_val(netE, netD, loader, criterion, device, trg_label=1):
     return total_clf_loss / n_val, total_d_loss / n_val, clf_cm, d_cm
 
 
+# for test (target)
+def eval_net_test(netE, loader, criterion, device):
+    netE.eval()
+
+    n_val = len(loader)  # the number of batch
+
+    total_clf_loss = 0
+
+    with tqdm(total=n_val, desc="Validation round", unit="batch", leave=False) as pbar:
+        for i, batch in enumerate(loader):
+            imgs, clf_labels = batch['image'], batch['label']
+            imgs = imgs.to(device=device, dtype=torch.float32)
+            clf_labels = clf_labels.to(device=device, dtype=torch.long)
+
+            with torch.no_grad():
+                clf_preds = netE(imgs, mode='class')
+                clf_loss = criterion(clf_preds, clf_labels)
+
+            total_clf_loss += clf_loss.item()
+
+            # confusion matrix
+            if i == 0:
+                clf_cm = make_confusion_matrix(clf_preds, clf_labels, cm=None)
+            else:
+                clf_cm = make_confusion_matrix(clf_preds, clf_labels, cm=clf_cm)
+
+            pbar.update()
+
+    netE.train()
+    return total_clf_loss / n_val, clf_cm
+
+
 # ========== for train_d.py ========== #
 # for train set (train_d)
 def eval_net_train_d(
@@ -184,33 +216,41 @@ def eval_net_train_d(
 
 
 # for validation sets (target, train_d)
-def eval_net_trg_val_d(netE, netD, loader, criterion, device, trg_label=1):
+def eval_net_trg_val_d(netE, netD, src_loader, trg_loader, criterion, device, src_label=0, trg_label=1):
     netE.eval()
     netD.eval()
 
-    n_val = len(loader)  # the number of batch
+    # n_val = len(src_loader)  # the number of batch
+    n_val = min(len(src_loader), len(trg_loader))
 
     total_d_loss = 0
 
     with tqdm(total=n_val, desc="Validation round", unit="batch", leave=False) as pbar:
-        for i, batch in enumerate(loader):
-            imgs = batch['image'].to(device=device, dtype=torch.float32)
-            d_trg_labels = torch.full((imgs.data.size()[0],), trg_label,
+        for i, (src_batch, trg_batch) in enumerate(zip(src_loader, trg_loader)):
+            src_imgs = src_batch['image'].to(device=device, dtype=torch.float32)
+            trg_imgs = trg_batch['image'].to(device=device, dtype=torch.float32)
+
+            d_src_labels = torch.full((src_imgs.data.size()[0],), src_label,
                                       dtype=torch.long, device=device)
+            d_trg_labels = torch.full((trg_imgs.data.size()[0],), trg_label,
+                                      dtype=torch.long, device=device)
+
+            imgs = torch.cat((src_imgs, trg_imgs), 0)
+            d_labels = torch.cat((d_src_labels, d_trg_labels), 0)
 
             with torch.no_grad():
                 featuremap = netE(imgs, mode='feature')
                 featuremap = featuremap.detach()
                 d_preds = netD(featuremap)
-                d_loss = criterion(d_preds, d_trg_labels)
+                d_loss = criterion(d_preds, d_labels)
 
             total_d_loss += d_loss.item()
 
             # confusion matrix
             if i == 0:
-                d_cm = make_confusion_matrix(d_preds, d_trg_labels, cm=None)
+                d_cm = make_confusion_matrix(d_preds, d_labels, cm=None)
             else:
-                d_cm = make_confusion_matrix(d_preds, d_trg_labels, cm=d_cm)
+                d_cm = make_confusion_matrix(d_preds, d_labels, cm=d_cm)
 
             pbar.update()
 
